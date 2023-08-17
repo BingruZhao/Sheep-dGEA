969a064fbb2050888de30229743c0d201e39a580
BingruZhao <142162887+BingruZhao@users.noreply.github.com>
2023-08-17 11:35:13 +0800

    Update 2_RNA-seq-splicing.sh

--- 2_RNA-seq-splicing.sh
+++ 2_RNA-seq-splicing.sh
@@ -3,23 +3,23 @@
 # Step 1. Converting bams to juncs
 for i in $(seq 1 10)
 do
-	sh leafcutter/scripts/bam2junc.sh liver${i}.bam liver${i}.junc
+	sh leafcutter/scripts/bam2junc.sh muscle${i}.bam muscle${i}.junc
 done
 
 
 # Step 2. Intron clustering
-ls *.junc > liver_juncfiles.txt
-python leafcutter/clustering/leafcutter_cluster.py -j liver_juncfiles.txt -m 50 -o liver -l 500000
+ls *.junc > muscle_juncfiles.txt
+python leafcutter/clustering/leafcutter_cluster.py -j muscle_juncfiles.txt -m 50 -o muscle -l 500000
 
 
 # Step 3. Calculate the PCA 
-python leafcutter/scripts/prepare_phenotype_table.py liver_perind.counts.gz -p 10 # note: -p 10 specific you want to calculate for sQTL to use as covariates
-sh liver_perind.counts.gz_prepare.sh
-head -6 liver_perind.counts.gz.PCs > liver_perind.counts.gz.PCs.PC5
+python leafcutter/scripts/prepare_phenotype_table.py muscle_perind.counts.gz -p 10 # note: -p 10 specific you want to calculate for sQTL to use as covariates
+sh muscle_perind.counts.gz_prepare.sh
+head -6 muscle_perind.counts.gz.PCs > muscle_perind.counts.gz.PCs.PC5
 
 
 # Step 4. sQTL detection
 for j in $(seq 1 29)
 do
-	fastQTL.static --vcf liver.filtered.vcf.gz --bed liver_perind.counts.gz.qqnorm_chr${j}.gz --cov liver_perind.counts.gz.PCs.PC5 --permute 1000 10000 --normal --out liver_perind.permutation.chr${j} --chunk 1 1
+	fastQTL.static --vcf muscle.filtered.vcf.gz --bed muscle_perind.counts.gz.qqnorm_chr${j}.gz --cov muscle_perind.counts.gz.PCs.PC5 --permute 1000 10000 --normal --out muscle_perind.permutation.chr${j} --chunk 1 1
 done
